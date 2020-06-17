d95365f0dfced906c5d2f5c377cdd3e179a48427
Ozal Khan <ozalkhan196@gmail.com>
2020-06-17 14:42:48 -0400

    able to login and use token to perform get request on auth only data on django server

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [blogs, setBlogs] = useState([]);
+  const [isAuth, setAuth] = useState(false);
+
+  const loadBooks = async () => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      try {
+        const { data } = await axios.get('http://127.0.0.1:8000/api/v1/', {
+          headers: {
+            Authorization: `Token ${token}`,
+          },
+        });
+        setBlogs(data);
+        console.log(data);
+      } catch (err) {
+        console.log(err);
+      }
+    } else {
+      alert('you must be logged in to see blogs');
+    }
+  };
+
+  const login = async () => {
+    try {
+      const { data } = await axios.post(
+        'http://127.0.0.1:8000/api/v1/dj-rest-auth/login/',
+        {
+          username: 'queenscoder',
+          email: 'queens@aol.com',
+          password: 'testingpasswordsaregood',
+        }
+      );
+      if (data.key) {
+        setAuth(true);
+        localStorage.setItem('token', data.key);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>start cool stuff here</h1>
+      <button onClick={loadBooks}>load blogs</button>
+      <br />
+      {!isAuth ? <button onClick={login}>Login</button> : <div>Hello User</div>}
+      <ul>
+        {blogs.map(({ id, body, author }) => {
+          return (
+            <li key={id}>
+              <h3>{body}</h3>
+              <h6>written by: {author}</h6>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
