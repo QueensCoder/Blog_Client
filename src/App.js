fc81d176ffa518cf6ec36e340ca38fbb0502c952
Ozal Khan <ozalkhan196@gmail.com>
2020-06-17 15:38:29 -0400

    need to fix logout so that it cannot work when a user has already logged out and delete the token as well

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import secrets from './secrets';
 
 const App = () => {
   const [blogs, setBlogs] = useState([]);
-  const [isAuth, setAuth] = useState(false);
+  const [isAuth, setAuth] = useState({});
+
+  // determine if user is authenticated or not
 
   const loadBooks = async () => {
     const token = localStorage.getItem('token');
@@ -40,29 +42,55 @@ const App = () => {
         }
       );
       if (data.key) {
-        setAuth(true);
+        console.log(data.key);
+        setAuth(data);
         localStorage.setItem('token', data.key);
+        console.log(localStorage.getItem('token'));
       }
     } catch (err) {
       console.log(err);
     }
   };
+
+  const logout = async () => {
+    const token = localStorage.getItem('token');
+    await axios.post('http://127.0.0.1:8000/api/v1/dj-rest-auth/logout/', {
+      headers: {
+        Authorization: `Token ${token}`,
+      },
+    });
+    localStorage.removeItem('token');
+    setAuth(false);
+  };
+
+  // list the blog content
+  const listBlogs = () => (
+    <ul>
+      {blogs.map(({ id, body, author }) => {
+        return (
+          <li key={id}>
+            <h3>{body}</h3>
+            <h6>written by: {author}</h6>
+          </li>
+        );
+      })}
+    </ul>
+  );
   return (
     <div className='App'>
       <h1>start cool stuff here</h1>
       <button onClick={loadBooks}>load blogs</button>
       <br />
-      {!isAuth ? <button onClick={login}>Login</button> : <div>Hello User</div>}
-      <ul>
-        {blogs.map(({ id, body, author }) => {
-          return (
-            <li key={id}>
-              <h3>{body}</h3>
-              <h6>written by: {author}</h6>
-            </li>
-          );
-        })}
-      </ul>
+      {!isAuth ? (
+        <button onClick={login}>Login</button>
+      ) : (
+        <div>
+          <h1>Hello user</h1>
+          <button onClick={logout}>Logout</button>
+          <br />
+          {listBlogs()}
+        </div>
+      )}
     </div>
   );
 };
